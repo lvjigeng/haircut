2ed4bceeafb8b44f5af6b8e187e9dcf069fa3515
wuxiaoyong <851780358@qq.com>
2018-02-28 10:05:43 +0800

    Users表控制器

---
+++ Application/Controller/Admin/UsersController.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 会员控制器
+ */
+class UsersController extends Controller
+{
+    //会员列表
+    public function index(){
+        //接收数据
+        //处理数据
+        //显示页面
+        $this->display("index");
+    }
+}
