f677142d53f4f64a4faca129483a26f5ad5f3f2e
lvjigeng <www.295533328@qq.com>
2018-02-28 13:03:07 +0800

    login

---
+++ Application/Controller/Admin/LoginController.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: www29
+ * Date: 2018/2/28
+ * Time: 11:00
+ */
+class LoginController extends Controller
+{
+    public function Login(){
+
+        $this->display('login');
+    }
+}
