11cc1a679495397b9e9af71b0dafb58741eae9a2
wuxiaoyong <851780358@qq.com>
2018-03-04 10:47:18 +0800

    会员分页修改

--- Application/Model/UsersModel.class.php
+++ Application/Model/UsersModel.class.php
@@ -19,7 +19,7 @@ class UsersModel extends Model
         $sql="select count(*) from users".$where;
 
         //每页显示6条记录
-        $pageSize=8;
+        $pageSize=5;
         //总记录数
         $count=$this->db->fetchColumn($sql);
         //总页数
